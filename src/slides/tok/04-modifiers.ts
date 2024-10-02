48c1490c9677e18b9707936f3d96d4a3b215b63c
Zachary Sakowitz <65452710+zsakowitz@users.noreply.github.com>
2024-10-01 23:11:07 -0700

    add names

--- src/slides/tok/04-modifiers.ts
+++ src/slides/tok/04-modifiers.ts
@@ -200,3 +200,20 @@ createReview`review: modifiers and "pp"`
       .tok`pb nimi pp pali ni`
       .eng`pp this activity's pb name`,
   )
+
+slide`activity: names`
+  .note`As an example, take ~"Bob". This is a person's name, so its _headnoun_ will be ~"jan". As for the name, ~"Po" or ~"Pa" works. Thus, the full name is ~"jan Po" or ~"jan Pa".`
+  .note`Let's try a country. Take China, for example. ~"China" is natively pronounced as ~"Zhōngguó", which would best be tokiponized as ~"Sonko". Note that ~"Sonkujo" might also work, but that increases the number of syllables, and is thus discouraged. China is a place, so we'll use ~"ma" as a headnoun for a final result of ~"ma Sonko".`
+  .note`Try converting your own name, and remember: you don't need to use a headnoun like ~"jan", even if you're a person. Play around with different headnouns and see which one fits best, or switch them around whever you feel like it!`
+  .note`And of course, you can break the rules around names to some degree. The primary author of this website goes by just ~"sakawi" or ~"sakisa", all lowercase, no headnoun. It's nonstandard, but plenty of people do it, and you can too!`(
+  ul`names in toki pona are like proper adjectives: they always attach to a base word`
+    .li`names are always capitalized (pali Sakisa, ma Mewika)`,
+  ul`names must follow toki pona's phonotactics (sound-rules):`,
+  ul`every syllable is a consonant, then a vowel, then an optional 'n'`
+    .li`the first syllable doesn't need an initial consonant`
+    .li`the clusters 'ji', 'ti', 'wo', 'wu', 'mn', and 'nn' are banned`,
+  ul`tips for converting names:`,
+  ul`'b d g z' become 'p t k s' respectively`
+    .li`names are based on pronunciation`
+    .li`keep syllable count constant by removing sounds if you have to`,
+)
